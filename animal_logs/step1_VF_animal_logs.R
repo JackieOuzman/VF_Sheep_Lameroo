d85cecefb7a3acd417cea2eeae86c72f5dc49bc0
Ouzman <ouz001@csiro.au>
2022-10-25 14:26:15 +1030

    still waiting on info from Damian and Rick

--- animal_logs/step1_VF_animal_logs.R
+++ animal_logs/step1_VF_animal_logs.R
@@ -92,13 +92,16 @@ animal_GPS_data_sf_trans <-
 rm(animal_GPS_data,animal_GPS_data_sf )
 
 
+
+
+
 ############################################################################################
 ############                  bring in boundaries             ##############################
 ############################################################################################
 
 
 
-#Lameroo_Vf_area_hard_fence_bound <- st_read(paste0(path_spatial, "pinnaroo_VF_boundary_dGPS_proj.shp"))  # this is the hard fences
+Lameroo_Vf_area_hard_fence_bound <- st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/HF_Lameroo_rough.shp")  # this is the hard fences
 
 # Lameroo_paddock_area <-             st_read("W:/VF/Pinnaroo 2022/Spatial/pinnaroo_boundary_dGPS_proj.shp")
 # Lameroo_Vf_area <-                  st_read("W:/VF/Pinnaroo 2022/Spatial/VF/VF_display_modified/all_VF.shp")
@@ -118,6 +121,15 @@ Lameroo_Vf_area <-
 
 
 
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  #geom_sf(data = pinnaroo_Vf_area, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = animal_GPS_data_sf_trans ,alpha = 0.01) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())
+#facet_wrap(. ~ Jax_fence_ID)
 
 
 
