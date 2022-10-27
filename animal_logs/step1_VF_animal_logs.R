455f132e5eaa06a705682a248485ad8e26cd20da
Ouzman <ouz001@csiro.au>
2022-10-27 11:22:29 +1030

    working out what data to keep

--- animal_logs/step1_VF_animal_logs.R
+++ animal_logs/step1_VF_animal_logs.R
@@ -11,21 +11,11 @@ library(sf)
 
 ### bring in animal logs for VF all
 
-
-#path_spatial <- "W:/VF/Pinnaroo 2022/Spatial/VF/VF_display_modified/"
-
 animal_GPS_data <- read_csv("W:/VF/Sheep_Lameroo_2022/animal_logs/raw_data/db_trial_csiro_lemaroo_mob_287_filtered.csv")
-#animal_GPS_data <- read_csv("W:/VF/Pinnaroo 2022/animal_log/raw_data_supplied/trial_csiro_pinnaroo_mob_273_angus_heifers_filtered.csv")
-
-
 #format time and date clm from character to time
 animal_GPS_data <-
   animal_GPS_data %>%
   mutate(timeOfEvent = as.POSIXct(timeOfEvent, tz = "GMT", format = "%d/%m/%Y %H:%M"))
-
-
-
-
 
 
 animal_GPS_data <- animal_GPS_data %>% 
@@ -33,10 +23,6 @@ animal_GPS_data <- animal_GPS_data %>%
 
 animal_GPS_data <- animal_GPS_data %>% 
   mutate(local_time = with_tz(GMT, tz = "Australia/Adelaide"))
-
-str(animal_GPS_data)
-
-names(animal_GPS_data)
 
 ## Add a clm for ID_jaxs
 animal_GPS_data <- animal_GPS_data %>% 
@@ -52,12 +38,6 @@ animal_GPS_data <- animal_GPS_data %>%
   dplyr::select(ID_jaxs,deviceUIDHex:local_time)
 
 
-### ----- this will need to be updated I have end time 12:00 on the 21st
-
-### filter data between two dates start and end of trial
-
-animal_GPS_data <- animal_GPS_data %>%  filter(local_time >= ymd_hms("2022-10-17 08:00:00", tz= "Australia/Adelaide"), #yyy-mm-dd hh:mm:ss
-                                               local_time <=  ymd_hms("2022-10-21 11:50:00", tz= "Australia/Adelaide"))
 
 animal_GPS_data <- animal_GPS_data %>% 
   mutate(date = as.Date(local_time, tz= "Australia/Adelaide"),
@@ -129,15 +109,8 @@ ggplot() +
   geom_sf(data = animal_GPS_data_sf_trans ,alpha = 0.01) +
   theme_bw()+
   theme(legend.position = "none",
-        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())
-#facet_wrap(. ~ Jax_fence_ID)
-
-
-
-
-
-names(animal_GPS_data_sf_trans)
-
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "all animal logs")
 
 
 
@@ -150,13 +123,18 @@ ggplot() +
   theme_bw()+
   theme(legend.position = "none",
         axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
-facet_wrap(. ~ date)
+facet_wrap(. ~ date)+
+  labs(title = "all animal logs, dates as facet")
 
-unique(animal_GPS_data_sf_trans$date)
+
+###### Need to work out what the start time 
+#Rick said there was a time the animals moved back in the VF area after they were in the south of the paddock and this is the satrt time
+
 GPS_data_sf_trans_17 <- animal_GPS_data_sf_trans %>% filter(date=="2022-10-17")
 
-
-#### ----- Up to HERE ----- ###### Need to work out what the start time is need to create new variable with time
+GPS_data_sf_trans_17 <- GPS_data_sf_trans_17 %>% 
+  mutate(hour = hour(local_time),
+         minute = minute(local_time))
 
 ggplot() +
   geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
@@ -166,7 +144,146 @@ ggplot() +
   geom_sf(data = GPS_data_sf_trans_17 ,alpha = 1) +
   theme_bw()+
   theme(legend.position = "none",
-        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  facet_wrap(.~ hour)+
+  labs(title = "Day 1, hours as facet")
+
+#so 11am to 14 looks like the window make 3 df for each of these hrs
+
+GPS_data_sf_trans_17_Hr_11 <- GPS_data_sf_trans_17 %>% 
+  filter(local_time >= ymd_hms("2022-10-17 11:00:00", tz= "Australia/Adelaide"), #yyy-mm-dd hh:mm:ss
+         local_time <=  ymd_hms("2022-10-17 12:00:00", tz= "Australia/Adelaide"))
+
+GPS_data_sf_trans_17_Hr_12 <- GPS_data_sf_trans_17 %>% 
+  filter(local_time >= ymd_hms("2022-10-17 12:00:00", tz= "Australia/Adelaide"), #yyy-mm-dd hh:mm:ss
+         local_time <=  ymd_hms("2022-10-17 13:00:00", tz= "Australia/Adelaide")) 
+
+GPS_data_sf_trans_17_Hr_13 <- GPS_data_sf_trans_17 %>% 
+  filter(local_time >= ymd_hms("2022-10-17 13:00:00", tz= "Australia/Adelaide"), #yyy-mm-dd hh:mm:ss
+         local_time <=  ymd_hms("2022-10-17 14:00:00", tz= "Australia/Adelaide"))  
+
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area_buffer_5, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = GPS_data_sf_trans_17_Hr_11 ,alpha = 1) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "Day 1 at 11am minute as facet")+
+  facet_wrap(.~ minute)
+  
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area_buffer_5, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = GPS_data_sf_trans_17_Hr_12 ,alpha = 1) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "Day 1 at 12am minute as facet")+
+  facet_wrap(.~ minute)
+
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area_buffer_5, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = GPS_data_sf_trans_17_Hr_12 ,alpha = 1) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "Day 1 at 12am minute as facet")+
+  facet_wrap(.~ minute)
+
+
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area_buffer_5, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = GPS_data_sf_trans_17_Hr_13 ,alpha = 1) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "Day 1 at 1pm minute as facet")+
+  facet_wrap(.~ minute)
+
+### so it look like the VF started at 13:10 prior to this the animals were training
+## the data will need a new clm for training and VF trial
+
+
+animal_GPS_data_sf_trans <- animal_GPS_data_sf_trans %>% 
+  mutate(training_period = case_when(
+    local_time <= ymd_hms("2022-10-17 13:10:00", tz= "Australia/Adelaide")~ "training",
+    TRUE                      ~ "non_training"
+    
+  ))
+
+
+### filter data between two dates start and end of trial
+
+animal_GPS_data_sf_trans <- animal_GPS_data_sf_trans %>%  filter(local_time >= ymd_hms("2022-10-17 10:00:00", tz= "Australia/Adelaide"), #yyy-mm-dd hh:mm:ss
+                                               local_time <=  ymd_hms("2022-10-21 11:50:00", tz= "Australia/Adelaide"))
+
+rm(GPS_data_sf_trans_17_Hr_13, GPS_data_sf_trans_17_Hr_12, GPS_data_sf_trans_17_Hr_11, GPS_data_sf_trans_17)
+
+
+
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area_buffer_5, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = animal_GPS_data_sf_trans ,alpha = 0.05) +
+  facet_wrap(.~ date)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "all animal logs between 17th at 10:30 and 21st at 11:50")
+
+
+
+
+animal_GPS_non_training <- animal_GPS_data_sf_trans %>% 
+  filter(training_period == "non_training")
+
+
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area_buffer_5, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = animal_GPS_non_training ,alpha = 0.05) +
+  facet_wrap(.~ date)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "all animal logs between 17th at 13:10 and 21st at 11:50",
+       subtitle = "non training")
+
+
+## what is the water point?
+list_of_deviceName <- unique(animal_GPS_non_training$deviceName)
+
+ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area_buffer_5, color = "black", fill = NA) +
+  #geom_sf(data = water_pts_sf ,color ="Blue") +
+  geom_sf(data = animal_GPS_non_training ,alpha = 0.05) +
+  facet_wrap(.~ deviceName)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "all animal logs between 17th at 13:10 and 21st at 11:50",
+       subtitle = "non training")
+
+
+#------------------UP TO HERE ------------------------------------##
+### I have asked Sue and Dana about the data 27/10/2022 waiting for resposne.
 
 
 ################################################################
