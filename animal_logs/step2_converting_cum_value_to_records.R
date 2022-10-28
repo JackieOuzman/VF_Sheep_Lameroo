9d8ce82bc23f77cb50cb94a9d375d04ce9fd402b
Ouzman <ouz001@csiro.au>
2022-10-28 16:09:59 +1030

    remove the cummulative values

--- animal_logs/step2_converting_cum_value_to_records.R
+++ animal_logs/step2_converting_cum_value_to_records.R
@@ -12,21 +12,21 @@ library(sf)
 
 path_output_files <- "W:/VF/Sheep_Lameroo_2022/animal_logs/jax_working/"
 VF_animals_logs <- read_csv(paste0(path_output_files,"animals_log_non_training.csv"))
-
+names(VF_animals_logs)
 
 ### check on records
 check_records <- VF_animals_logs %>% 
-  group_by( Sheep_ID , date) %>% 
+  group_by( Sheep_ID, deviceName , date) %>% 
   summarise(n_AudioCount = sum(cumulativeAudioCount),
             n_ShockCount = sum(cumulativeShockCount)
   )
 
 check_records <- check_records %>% 
-  arrange( Sheep_ID,date, n_AudioCount )
+  arrange( Sheep_ID,date,deviceName, n_AudioCount )
 
 ### check on values
 check_min_max <- VF_animals_logs %>% 
-  group_by( Sheep_ID , date) %>% 
+  group_by( Sheep_ID , deviceName, date) %>% 
   summarise(min_AudioCount = min(cumulativeAudioCount,na.rm = TRUE),
             max_AudioCount = max(cumulativeAudioCount,na.rm = TRUE),
             min_ShockCount = sum(cumulativeShockCount,na.rm = TRUE),
@@ -34,9 +34,9 @@ check_min_max <- VF_animals_logs %>%
   )
 
 check_min_max <- check_min_max %>% 
-  arrange(Sheep_ID,date, min_AudioCount )
+  arrange(Sheep_ID,deviceName,date, min_AudioCount )
 
-### for fence 1 there are 35 cows all with records more than 1 and max value more than 0
+
 
 
 #make a list###
@@ -47,10 +47,9 @@ list_animals <- VF_animals_logs %>%
   select(deviceName) %>% 
   distinct(deviceName)
 list_animals <-ungroup(list_animals)
+list_animals
 
 
-list_animals <- list_animals %>% 
-  filter(deviceName != "1390310")
 
 test <- list_animals
 #list_animals <- head(list_animals,20)
@@ -59,40 +58,18 @@ test <- list_animals
 
 list_animals$deviceName[1:34]
 list_animals <- c(
-1390038, 
-1390063, 
-1390068, 
-1390103, 
-1390139, 
-1390171, 
-1390196, 
-1390221, 
-1390305,
-1390416, 
-1390456, 
-1390495, 
-1390560, 
-1390577, 
-1390581, 
-1390743, 
+ 490705,
+1390182,
+1390456,
+1390577,
+1390581,
+1390737,
+1390743,
 1390749,
-1390775, 
-1390826, 
-1390832, 
-1390858, 
-1390889, 
-1391048, 
-1391200, 
-1391209, 
-1391211, 
-1391234, 
-1391339, 
-1391387, 
-1391502, 
-1391505, 
-1391517, 
-1391842, 
-1391796)
+1390736,
+1390826,
+1391505
+  )
 
 
 
@@ -101,19 +78,18 @@ for (list_animals in list_animals){
   
   ##################################################################################################################
   
-  #fence_x <- sub("_.*", "", list_animals)
-  fence_x <- 1
+  
   deviceName_x <- sub("*._", "", list_animals)
   
   df <- VF_animals_logs %>% 
-    #filter(Jax_fence_ID == fence_x) %>% 
     filter(deviceName == deviceName_x)
+    #filter(deviceName == 490705)
   
   df <- df %>% arrange(local_time)
-  
+  names(df)
   df <- df %>% 
     mutate(Audio_values = cumulativeAudioCount - lag(cumulativeAudioCount))
-  
+  #test <- df %>%  select(deviceName, local_time, cumulativeAudioCount, Audio_values)
   df <- df %>% 
     mutate(Shock_values = cumulativeShockCount - lag(cumulativeShockCount))
   
@@ -128,78 +104,42 @@ for (list_animals in list_animals){
 
 
 Fence_all <- rbind(
-  temp_1390038, 
-  temp_1390063, 
-  temp_1390068, 
-  temp_1390103, 
-  temp_1390139, 
-  temp_1390171, 
-  temp_1390196, 
-  temp_1390221, 
-  temp_1390305,
-  temp_1390416, 
-  temp_1390456, 
-  temp_1390495, 
-  temp_1390560, 
-  temp_1390577, 
-  temp_1390581, 
-  temp_1390743, 
+  temp_490705,
+  temp_1390182,
+  temp_1390456,
+  temp_1390577,
+  temp_1390581,
+  temp_1390737,
+  temp_1390743,
   temp_1390749,
-  temp_1390775, 
-  temp_1390826, 
-  temp_1390832, 
-  temp_1390858, 
-  temp_1390889, 
-  temp_1391048, 
-  temp_1391200, 
-  temp_1391209, 
-  temp_1391211, 
-  temp_1391234, 
-  temp_1391339, 
-  temp_1391387, 
-  temp_1391502, 
-  temp_1391505, 
-  temp_1391517, 
-  temp_1391842, 
-  temp_1391796)
-rm(
-  "temp_1390038", 
-  "temp_1390063", 
-  "temp_1390068", 
-  "temp_1390103", 
-  "temp_1390139", 
-  "temp_1390171", 
-  "temp_1390196", 
-  "temp_1390221", 
-  "temp_1390305",
-  "temp_1390416", 
-  "temp_1390456", 
-  "temp_1390495", 
-  "temp_1390560", 
-  "temp_1390577", 
-  "temp_1390581", 
-  "temp_1390743", 
-  "temp_1390749",
-  "temp_1390775", 
-  "temp_1390826", 
-  "temp_1390832", 
-  "temp_1390858", 
-  "temp_1390889", 
-  "temp_1391048", 
-  "temp_1391200", 
-  "temp_1391209", 
-  "temp_1391211", 
-  "temp_1391234", 
-  "temp_1391339", 
-  "temp_1391387", 
-  "temp_1391502", 
-  "temp_1391505", 
-  "temp_1391517", 
-  "temp_1391842", 
-  "temp_1391796")
+  temp_1390736,
+  temp_1390826,
+  temp_1391505
+)
+rm( temp_490705,
+    temp_1390182,
+    temp_1390456,
+    temp_1390577,
+    temp_1390581,
+    temp_1390737,
+    temp_1390743,
+    temp_1390749,
+    temp_1390736,
+    temp_1390826,
+    temp_1391505
+  )
 
-output_path <- "W:/VF/Pinnaroo 2022/animal_log/jax_working_outputs"
 
+path_output_files
 write.csv(Fence_all, 
-          paste0(output_path,"/animal_GPS_data_clip_uncum_step2.csv"), 
+          paste0(path_output_files,"/animal_GPS_data_nonTrain_step1_2.csv"), 
           row.names=FALSE)
+
+names(Fence_all)
+
+check <-
+  Fence_all %>% select(deviceName,
+                       Audio_values  ,
+                       Shock_values, 
+                       local_time ,  
+                       Sheep_ID   )
