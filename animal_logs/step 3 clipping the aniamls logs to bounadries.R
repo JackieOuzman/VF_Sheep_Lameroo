a51455ae5d0890fbd78ec70504c47d14697ddb5e
Ouzman <ouz001@csiro.au>
2022-11-03 13:11:09 +1030

    small changes splitting analysis

---
+++ animal_logs/step 3 clipping the aniamls logs to bounadries.R
@@ -0,0 +1,60 @@
+## step 3 clipping the data - This has not been run yet
+
+library(dplyr)
+library(tidyverse)
+library(readr)
+library(lubridate)
+library(DT)
+library(sp)
+#install.packages("sf")
+library(sf)
+
+
+############################################################################################
+############                  bring in boundaries             ##############################
+############################################################################################
+
+
+
+Lameroo_Vf_area_hard_fence_bound <- st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/VF_working/HF_Lameroo_rough_proj.shp")  # this is the hard fences
+Lameroo_Vf_area_hard_fence_bound_buff <- st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/HF_Lameroo_rough_10_proj.shp")  # this is the 
+
+Lameroo_Vf_area <-                  st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/VF_proj.shp")
+Lameroo_Vf_area_buffer_10 <-                  st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/VF_Buffer10_proj.shp")
+water_pt <-  st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/water_pts.shp")
+
+
+
+
+
+Lameroo_Vf_area_hard_fence_bound <-
+  st_transform(Lameroo_Vf_area_hard_fence_bound, crs = 28354)
+
+Lameroo_Vf_area_buffer_10 <-
+  st_transform(Lameroo_Vf_area_buffer_10, crs = 28354)
+Lameroo_Vf_area <-
+  st_transform(Lameroo_Vf_area, crs = 28354)
+water_pt <-
+  st_transform(water_pt, crs = 28354)
+
+Lameroo_Vf_area_hard_fence_bound_buff<-
+  st_transform(Lameroo_Vf_area_hard_fence_bound_buff, crs = 28354)
+
+
+
+
+################################################################
+### Clip to the VF hard fences  with 10 meter buffer   #########
+################################################################
+
+
+#To the large block boundary
+animals_GPS_trim_time_clip <-
+  st_intersection(animals_GPS_trim_time, Lameroo_Vf_area_hard_fence_bound_buff)
+
+
+## remove all the rows that don't have fence ID
+#unique(animal_GPS_data_sf_trans_clip$fencesID)
+animals_GPS_trim_time_clip <-animals_GPS_trim_time_clip %>%
+  filter(!is.na(fencesID) ) %>%
+  filter(fencesID !=  "NULL")
