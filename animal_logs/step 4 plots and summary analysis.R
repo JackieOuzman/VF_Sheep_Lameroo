4a4d78c10a5dc4dbebfe6c224af96432e0a02b39
Ouzman <ouz001@csiro.au>
2022-11-07 15:07:18 +1030

    hopefully all that is required

--- animal_logs/step 4 plots and summary analysis.R
+++ animal_logs/step 4 plots and summary analysis.R
@@ -71,18 +71,15 @@ check <-
 
 ## TASK Mean audio/pulse ratio AIM: Across all animals
 names(step1_2_3_sf)
-
+# step 1 summaries audio and pulse per animal per day also training period 
 summary_audio_ratio <- step1_2_3_sf %>% 
-  group_by(Sheep_ID, date, training_period) %>% 
-  summarise(audio_sum = sum(Audio_values, na.rm = TRUE),
+  dplyr::group_by(Sheep_ID, date, training_period) %>% 
+  dplyr::summarise(audio_sum = sum(Audio_values, na.rm = TRUE),
             pulse_sum = sum(Shock_values, na.rm = TRUE),
             ratio_sum1 = audio_sum/ (pulse_sum+audio_sum )*100,
             ratio_sum2 = pulse_sum/ (audio_sum )*100)
   
-summary_audio_ratio <- summary_audio_ratio %>% 
-  ungroup(summary_audio_ratio) %>% 
-  dplyr::select(-geometry)
-names(summary_audio_ratio)
+summary_audio_ratio <- ungroup(summary_audio_ratio)
 
 summary_audio_ratio$ratio_sum1 [is.nan(summary_audio_ratio$ratio_sum1 )]<-NA
 summary_audio_ratio$ratio_sum2 [is.nan(summary_audio_ratio$ratio_sum2 )]<-NA
@@ -99,47 +96,160 @@ summary_audio_ratio %>%
   labs(
     x = "Date",
     y = "ratio (Audio / pulse+audio)*100 ",
-    title = paste("Animals fitted with VF collars")
-  )
+    title = "Animals fitted with VF collars in non training period",
+    subtitle = "audio and pulse counts summed per day and animal and then ratio calulated")
+  
+summary_audio_ratio
 
-
-
+# step 2 summaries audio and pulse per animal per day also training period 
 
 summary_audio_ratio_from_training <- summary_audio_ratio %>% 
-  group_by(training_period) %>% 
-  summarise(audio_av = mean(audio_sum, na.rm = TRUE),
+  dplyr::group_by(training_period) %>% 
+  dplyr::summarise(audio_av = mean(audio_sum, na.rm = TRUE),
             pulse_av = mean(pulse_sum, na.rm = TRUE),
-            ratio_1_mean = mean(ratio_sum1, na.rm= TRUE),
-            ratio_2_mean = mean(ratio_sum2, na.rm= TRUE))
             
+            std_dev_Av_Audio = sd(audio_sum, na.rm = TRUE),
+            SE_Av_Audio = std_dev_Av_Audio / sqrt(n()),
+            
+            std_dev_Av_Pulse = sd(pulse_sum, na.rm = TRUE),
+            SE_Av_Pulse = std_dev_Av_Pulse / sqrt(n()),
+            
+            ratio_1_mean = mean(ratio_sum1, na.rm= TRUE),
+            ratio_2_mean = mean(ratio_sum2, na.rm= TRUE),
+            
+            std_dev_Av_Ratio_1 = sd(ratio_sum1, na.rm = TRUE),
+            SE_Av_std_dev_Av_Ratio_1 = std_dev_Av_Ratio_1 / sqrt(n()),
+            
+            std_dev_Av_Ratio_2 = sd(ratio_sum2, na.rm = TRUE),
+            SE_Av_std_dev_Av_Ratio_2 = std_dev_Av_Ratio_2 / sqrt(n())
+            
+            )
+  
 
-summary_audio_ratio_from_training %>%
-  ggplot(aes(x = training_period , y = audio_av)) +
-  geom_col()+
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 90))+
-  #geom_vline(xintercept = as.Date(vertical_lines), col = "blue")+
-  labs(
-    x = "Traing period",
-    y = "Avearge Audio ",
-    title = paste("Average Audio in training period and during trial")
-  )
+summary_audio_ratio_from_training$training_period <- factor(summary_audio_ratio_from_training$training_period,
+                                                            levels = c("training", "non_training"))    
 
-
-################################################################################
-## per animal per day what is the total (sum) and then what is the ratio
-names(Fence_all)
-
-non_train_summary <-  Fence_all %>% 
-  group_by(Sheep_ID, date) %>% 
-  summarise(sum_aduio = sum(Audio_values, na.rm = TRUE),
-            sum_pulse = sum(Shock_values, na.rm = TRUE),
-            ratio = (sum_aduio/(sum_pulse+ sum_aduio))*100)
-non_train_summary$ratio <-   round(non_train_summary$ratio ,2)
-
-non_train_summary$ratio [is.nan(non_train_summary$ratio )]<-NA
+names(summary_audio_ratio_from_training)
 
 
-write.csv(non_train_summary, 
-          paste0(path_output_files,"/summary_nonTrain.csv"), 
-          row.names=FALSE)
+summary_audio_ratio_from_training <- as.data.frame(summary_audio_ratio_from_training)
+
+summary_audio_ratio_from_training <- summary_audio_ratio_from_training %>%dplyr::select(training_period ,
+                audio_av ,  
+                pulse_av  ,   
+                std_dev_Av_Audio ,
+                SE_Av_Audio , 
+                std_dev_Av_Pulse ,
+                SE_Av_Pulse,  
+                ratio_1_mean  ,
+                ratio_2_mean , 
+                std_dev_Av_Ratio_1  ,    
+                SE_Av_std_dev_Av_Ratio_1 , 
+                std_dev_Av_Ratio_2,
+                SE_Av_std_dev_Av_Ratio_2)
+
+
+DT::datatable(summary_audio_ratio_from_training ,
+              rownames = FALSE,  
+              options = list(columnDefs =
+                               list(list(className = 'dt-center',
+                                         targets = "_all")))) %>%
+  formatRound(c(2:13), 2) #this round clm number  to 2 decimal places
+  
+
+
+
+
+
+
+## GPS Plots
+
+#AIM: Daily plots are probably sufficient. 
+
+#But more frequent plots during the training period.
+#And there was that instance where they broke through the fence overnight (we think something spooked them), 
+#so a more detailed breakdown of that could also be interesting too. 
+names(step1_2_3_sf)
+step1_2_3_sf_trial_only <- step1_2_3_sf %>% 
+  filter(training_period == "non_training")
+
+
+
+plot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = water_pt ,color ="Blue") +
+  geom_sf(data = step1_2_3_sf_trial_only ,alpha = 0.05) +
+  facet_wrap(.~ date)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "Animal logs in during trial",
+       subtitle = "log when animals were yarded removed, and clipped to 10 meter buffer")
+
+
+### DAY 1 only 
+
+
+step1_2_3_sf_day1 <- step1_2_3_sf %>% filter(date=="2022-10-17")
+
+step1_2_3_sf_day1 <- step1_2_3_sf_day1 %>% 
+  mutate(hour = hour(local_time),
+         minute = minute(local_time))
+
+step1_2_3_sf_day1$training_period <- factor(step1_2_3_sf_day1$training_period,
+                                                            levels = c("training", "non_training"))    
+
+
+day1 <- ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = water_pt ,color ="Blue") +
+  geom_sf(data = step1_2_3_sf_day1 ,alpha = 0.08) +
+    theme_bw()+
+  facet_wrap(training_period~ hour)+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "Animal logs on first day")
+
+ggsave(day1,
+       device = "png",
+       filename = paste0("Lameroo_sheep_day1.png"),
+       path= "W:/VF/Sheep_Lameroo_2022/R_scripts/plots/",
+       width=8.62,
+       height = 6.28,
+       dpi=600
+)
+
+
+
+step1_2_3_sf_day4 <- step1_2_3_sf %>% filter(date=="2022-10-20")
+
+step1_2_3_sf_day4 <- step1_2_3_sf_day4 %>% 
+  mutate(hour = hour(local_time),
+         minute = minute(local_time))
+
+step1_2_3_sf_day4$training_period <- factor(step1_2_3_sf_day4$training_period,
+                                            levels = c("training", "non_training"))    
+
+
+day4 <-ggplot() +
+  geom_sf(data = Lameroo_Vf_area_hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = Lameroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = water_pt ,color ="Blue") +
+  geom_sf(data = step1_2_3_sf_day4 ,alpha = 0.08) +
+  theme_bw()+
+  facet_wrap(.~ hour)+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title = "Animal logs on 4th day")
+
+
+ggsave(day4,
+       device = "png",
+       filename = paste0("Lameroo_sheep_day4.png"),
+       path= "W:/VF/Sheep_Lameroo_2022/R_scripts/plots/",
+       width=8.62,
+       height = 6.28,
+       dpi=600
+)
